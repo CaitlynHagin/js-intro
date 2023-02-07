aaa1a456e35dc7276b3a9bfde0837f3cbe0ca376
CaitlynHagin <caitlyn32h@gmail.com>
2023-02-07 16:24:15 +0000

    actual BMI FIXED

---
+++ BMI/script.js
@@ -0,0 +1,43 @@
+    //showing my js is linked to html
+    console.log("am linked test")
+
+
+   //linking button with id - getting the element 
+    let button =document.getElementById('calculate')
+
+    //listening for click on button
+    button.addEventListener('click',(e)=> { 
+
+    //stopping browser from refreshing 
+    e.preventDefault()
+
+    // creating new variable, assigning it to element to get the value
+    let height = document.getElementById("height").value 
+
+    // creating another variable, assigning element to get another value
+    let weight = document.getElementById("weight").value 
+
+    // checking values are being assigned 
+    console.log(height,weight)
+
+    // new variable called squareHeight - which is height squared 
+    let squareHeight=height*height
+
+    // checking height squared works 
+    console.log(squareHeight)
+
+    //new variable called bmi, calculating weight/squared height
+    let bmi=weight/squareHeight
+
+    //check the calculation works
+    console.log(bmi)
+
+    //new variable and assigning it result from html page
+    let result = document.getElementById("result")
+
+    //showing the user the result, changing result in html
+    result.innerHTML="Your BMI is:    "  + bmi
+})
+
+
+    
---
+++ helloworld.js
@@ -0,0 +1,20 @@
+console.log('sending a message');
+
+// document.getElementById("first-para").innerHTML ='Hello!';
+
+/*TUESDAY*/
+
+const mydiv = document.getElementById("example");
+const myinput = document.getElementById("myinput");
+const mylink = document.getElementById("mylink");
+
+// console.log(mydiv.style.backgroundColor);
+console.log(myinput.value);
+console.log(mylink.href);
+// this is an instruction to change the href attribute to airbnb.co.uk
+mylink.href = 'https://airbnb.co.uk';
+myinput.value = 'Dummy text';
+mydiv.style.backgroundColor = 'blue';
+/* create three elements on the page, each with an ID of your choice
+Using document.getElement... change some attribute on the element */
+// white_check_mark
